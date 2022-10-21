4321696b1ac436ae5892234a84635ea7c5d69687
Aram <aramhrptn@hotmail.com>
2022-10-22 02:04:49 +0400

    added documentation

--- picker.rb
+++ picker.rb
@@ -6,13 +6,13 @@ require 'logger'
 class Picker
   def self.from(count, value) = Array.new(count).fill(value)
 
-  attr_reader :disk_count,
-              :from,
-              :to,
-              :skip,
-              :logger,
-              :result,
-              :up_to
+  attr_reader :disk_count, # Count of the locker disks
+              :from, # initial position of the locker
+              :to, # position to open the locker
+              :skip, # skip specified positions
+              :logger, # just a plain old logger
+              :result, # an array of arrays of integers, where we store all the positions we've been through
+              :up_to # max value of the wheel
 
   def initialize(
     disk_count: 6,
@@ -33,7 +33,7 @@ class Picker
   def pick
     logger.info("Picking from #{from} to #{to} skipping #{skip}")
 
-    result << from
+    result << from # push initial value of the locker to the results array
 
     pick!(-1) until from == to
 
@@ -47,17 +47,18 @@ class Picker
   def pick!(index = -1)
     return if index < @from.count * -1 || @from == to
 
-    dup_lock = @from.dup
+    dup_lock = @from.dup # duplicate the locker in case we need to revert the changes
 
-    dup_lock[index] = (dup_lock[index] % up_to) + 1 if dup_lock[index] < to[index]
+    dup_lock[index] = (dup_lock[index] % up_to) + 1 if dup_lock[index] < to[index] # increment the value of the wheel
+    # until maximum value is reached, then just roll to 0
 
     unless skip.include?(dup_lock)
       result << dup_lock unless result.include?(dup_lock)
 
-      @from = dup_lock
+      @from = dup_lock # if the new value is not in the skip list, then we can assign it to the locker
     end
 
-    pick!(index - 1)
+    pick!(index - 1) # recursively call the method until we reach the end of the locker
 
     nil
   end
---
+++ readme.md
@@ -0,0 +1,8 @@
+[![forthebadge](https://forthebadge.com/images/badges/certified-elijah-wood.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/fuck-it-ship-it.svg)]()
+
+# HOW TO
+* install `ruby 3.x.x`
+* run `bundle install`
+* run `rake run`
+
